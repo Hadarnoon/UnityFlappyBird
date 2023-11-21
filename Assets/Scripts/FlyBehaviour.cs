032c8edb602ee6edfb066e20866ba289c9037008
Oszlik Tamás <70895240+Hadarnoon@users.noreply.github.com>
2023-11-21 09:07:48 +0100

    init

---
+++ Assets/Scripts/FlyBehaviour.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FlyBehaviour : MonoBehaviour
+{
+    [SerializeField] private float _velocity = 1.5f;
+    [SerializeField] private float _rotationSpeed = 10f;
+
+    private Rigidbody2D _rigidBody2;
+     private void Start()
+    {
+        _rigidBody2 = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            _rigidBody2.velocity = Vector2.up * _velocity;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, _rigidBody2.velocity.y * _rotationSpeed);
+    }
+}
