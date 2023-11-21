582f4f4c820a1e103c6d1046d7c8b5f7a0c7e0e6
Oszlik Tamás <70895240+Hadarnoon@users.noreply.github.com>
2023-11-21 10:25:04 +0100

    masodik push

---
+++ Assets/Scripts/MovePipe.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePipe : MonoBehaviour
+{
+    [SerializeField] float _speed = 0.65f;
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.position += Vector3.left * _speed * Time.deltaTime;
+    }
+}
